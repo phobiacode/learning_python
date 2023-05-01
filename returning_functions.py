3db6d38f148c0b5d576a3a714fc8715f93eef569
phobiaCode <41870151+phobiacode@users.noreply.github.com>
2023-05-01 17:26:45 -0400

    Add files via upload

---
+++ returning_functions.py
@@ -0,0 +1,26 @@
+
+"""Returning Functions"""
+
+
+def math_operation(operation):
+    def add(x, y):
+        return x + y
+
+    def sub(x, y):
+        return x - y
+
+    if operation == '+':
+        return add
+    elif operation == '-':
+        return sub
+    else:
+        print('Invalid Input')
+
+
+addition = math_operation('+')
+subtraction = math_operation('-')
+
+print('Returning Functions:')
+print(addition(5, 7), subtraction(5, 7))
+
+
